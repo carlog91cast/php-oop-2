ad0313d527376c01026205cfe6c864c6d07eb527
bigeppo <carlog91cast@gmail.com>
2022-09-05 14:56:20 +0200

    add user class

---
+++ classes/user.php
@@ -0,0 +1,36 @@
+<?php
+class user {
+    protected $name;
+    protected $mail;
+    protected $cart_date;
+    
+    protected $choosenProducts = [];
+
+    function __construct($_name,$_mail,$_cart_date)
+    {
+        $this->name = $_name;
+        $this->mail = $_mail;
+        $this->cart_date = $_cart_date;
+    }
+
+    public function GetName(){
+        return $this->name;
+    }
+
+    public function GetMail(){
+        return $this->mail;
+    }
+    public function GetCartDate(){
+        return $this->cart_date;
+    }
+
+    public function addProds($product){
+        $this->choosenProducts[] = $product;
+    }
+}
+
+
+
+
+
+?>
