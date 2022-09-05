3108352425fe538755931c488266ac4513a971e3
bigeppo <carlog91cast@gmail.com>
2022-09-05 17:46:24 +0200

    add cleaning product class

---
+++ classes/cleaning_prod.php
@@ -0,0 +1,32 @@
+<?php 
+
+include_once __DIR__ . '/products.php';
+class games extends products{
+    protected $weight;
+    protected $bodyPart;
+
+    function  __construct($_name, $_brand, $_animalGenre,$_weight,$_bodyPart){
+        parent:: __construct($_name, $_brand, $_animalGenre);
+        $this->weight = $_weight;
+        $this->bodyPart = $_bodyPart;
+    
+    }
+    public function getWeight(){
+        return $this->weight;
+    }
+    public function setWeight($_weight){
+        $this->weight = $_weight;
+    }
+    public function getBodyPart(){
+        return $this->bodyPart;
+    }
+    public function setBodyPart($_bodyPart){
+        $this->bodyPart = $_bodyPart;
+    }
+}
+
+
+
+
+
+?>
